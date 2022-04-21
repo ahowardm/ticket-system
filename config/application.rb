549c961bf2b34b408a10336d653834793a5d9d83
Andres Howard <ahowardm@gmail.com>
2022-04-21 17:06:28 -0400

    feat: add project locales

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,11 @@ module TicketSystem
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "America/Santiago"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = 'es'
+    config.i18n.fallbacks = ['es']
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'models', '**', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'views', '**', '*.yml').to_s]
   end
 end
