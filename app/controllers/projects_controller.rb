0819b8a9ac2338198accd81e532a4e36df1a7192
Andres Howard <ahowardm@gmail.com>
2022-04-21 17:07:40 -0400

    feat: add basic actions for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,20 @@
+class ProjectsController < ApplicationController
+    def index
+        @projects = Project.all
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        project = Project.create! project_params
+        redirect_to projects_path
+    end
+
+    private
+
+        def project_params
+            params.require(:project).permit(:name)
+        end
+end
