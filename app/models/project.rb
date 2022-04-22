2ddb5ea87697619f90f7d2d79cbd58f5c1cb6492
Andres Howard <ahowardm@gmail.com>
2022-04-22 00:22:11 -0400

    refactor: change name in project to string

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@
 # Table name: projects
 #
 #  id         :bigint           not null, primary key
-#  name       :text
+#  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_22_015212) do
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
