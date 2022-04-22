aa9dd86f833e6232bff536c3d6034990b6b87897
Andres Howard <ahowardm@gmail.com>
2022-04-21 21:42:14 -0400

    style: fix tabs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,38 +1,38 @@
 class ProjectsController < ApplicationController
-    before_action :find_project, only: [:edit, :update, :destroy]
+  before_action :find_project, only: [:edit, :update, :destroy]
 
-    def index
-        @projects = Project.all
-    end
+  def index
+      @projects = Project.all
+  end
 
-    def new
-        @project = Project.new
-    end
+  def new
+      @project = Project.new
+  end
 
-    def create
-        project = Project.create! project_params
-        redirect_to projects_path
-    end
-
-    def edit ; end
-
-    def update
-      @project.update project_params
+  def create
+      project = Project.create! project_params
       redirect_to projects_path
+  end
+
+  def edit ; end
+
+  def update
+    @project.update project_params
+    redirect_to projects_path
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path, notice: 'Proyecto eliminado con éxito'
+  end
+
+  private
+
+    def project_params
+        params.require(:project).permit(:name)
     end
 
-    def destroy
-      @project.destroy
-      redirect_to projects_path, notice: 'Proyecto eliminado con éxito'
+    def find_project
+      @project = Project.find params[:id]
     end
-
-    private
-
-        def project_params
-            params.require(:project).permit(:name)
-        end
-
-        def find_project
-          @project = Project.find params[:id]
-        end
 end
